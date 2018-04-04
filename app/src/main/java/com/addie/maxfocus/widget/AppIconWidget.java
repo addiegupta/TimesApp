ec3d56cf6a16bc74c7376e62d4daef32569d191a
Aditya <addiegupta@gmail.com>
2018-04-04 23:47:47 +0530

    Add widget to start chosen app with icon.

---
+++ app/src/main/java/com/addie/maxfocus/widget/AppIconWidget.java
@@ -0,0 +1,92 @@
+package com.addie.maxfocus.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.BitmapDrawable;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import com.addie.maxfocus.R;
+
+/**
+ * Implementation of App Widget functionality.
+ * App Widget Configuration implemented in {@link AppIconWidgetConfigureActivity AppIconWidgetConfigureActivity}
+ */
+public class AppIconWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        String packageName = AppIconWidgetConfigureActivity.loadPackageNameForWidget(context, appWidgetId);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.layout_widget_icon);
+        try {
+            views.setImageViewBitmap(R.id.widget_icon, ((BitmapDrawable) context.getPackageManager().getApplicationIcon(packageName)).getBitmap());
+
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        // Broadcast intent with selected time for app to be stopped
+//            int time = minutes * 60000;
+//            Intent broadcastIntent = new Intent();
+//            broadcastIntent.putExtra(TIME_KEY, time);
+//            broadcastIntent.putExtra(TARGET_PACKAGE_KEY, mTargetPackage);
+//            broadcastIntent.setAction(ACTION_APP_DIALOG);
+
+//            context.sendBroadcast(broadcastIntent);
+
+
+//        TODO: FIX!! Causing crash
+        if (!packageName.equals(context.getString(R.string.appwidget_text))) {
+            // Launches the selected app
+            PackageManager packageManager = context.getPackageManager();
+
+            Intent launchIntent = packageManager.getLaunchIntentForPackage(packageName);
+            if (launchIntent != null) {
+                launchIntent.setAction(Long.toString(System.currentTimeMillis()));
+//                launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            }
+
+            Log.d("WIDGET", "Package being applied in intent is :" + packageName);
+            PendingIntent appPendingIntent = PendingIntent.getActivity(context, 0, launchIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            views.setOnClickPendingIntent(R.id.widget_icon, appPendingIntent);
+        }
+
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        // When the user deletes the widget, delete the preference associated with it.
+        for (int appWidgetId : appWidgetIds) {
+            AppIconWidgetConfigureActivity.deleteTitlePref(context, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
---
+++ app/src/main/java/com/addie/maxfocus/widget/AppIconWidgetConfigureActivity.java
@@ -0,0 +1,358 @@
+package com.addie.maxfocus.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.addie.maxfocus.R;
+import com.addie.maxfocus.adapter.AppAdapter;
+import com.addie.maxfocus.data.AppColumns;
+import com.addie.maxfocus.model.App;
+import com.addie.maxfocus.receiver.AppDialogBroadcastReceiver;
+import com.addie.maxfocus.ui.MainActivity;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import timber.log.Timber;
+
+import static com.addie.maxfocus.data.AppProvider.Apps.URI_APPS;
+
+
+//TODO:Refactor and check everything carefully
+/**
+ * The configuration screen for the {@link AppIconWidget AppIconWidget} AppWidget.
+ */
+public class AppIconWidgetConfigureActivity extends AppCompatActivity implements AppAdapter.AppOnClickHandler {
+
+    private static final String ACTION_APP_DIALOG = "com.addie.maxfocus.service.action.APP_DIALOG";
+    private static final int APPS_LOADER_MANAGER_ID = 131;
+    private static final int APPS_LOADER_DB_ID = 486;
+    @BindView(R.id.rv_widget_apps)
+    RecyclerView mAppsRecyclerView;
+    @BindView(R.id.pb_widget_apps_loading_indicator)
+    ProgressBar mLoadingIndicator;
+
+    private AppAdapter mAdapter;
+    private App mSelectedApp;
+    private AppDialogBroadcastReceiver mAppDialogBroadcastReceiver;
+    private static PackageManager mPackageManager;
+
+
+    private void loadAppsFromManagerOrDb() {
+        Cursor cursor = getContentResolver().query(URI_APPS, null, null, null, null);
+        if (cursor != null && cursor.getCount() != 0) {
+            getSupportLoaderManager().initLoader(APPS_LOADER_DB_ID, null, fetchAppsListener);
+        } else {
+            getSupportLoaderManager().initLoader(APPS_LOADER_MANAGER_ID, null, fetchAppsListener);
+        }
+        if (cursor != null) {
+
+            cursor.close();
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mAppDialogBroadcastReceiver);
+    }
+
+    /**
+     * Called by AppAdapter when an app is selected in the RecyclerView
+     *
+     * @param selectedApp the app that is selected
+     */
+    @Override
+    public void onClick(App selectedApp) {
+        mSelectedApp = selectedApp;
+
+        final Context context = AppIconWidgetConfigureActivity.this;
+
+        // When the button is clicked, store the string locally
+        String packageName = mSelectedApp.getmPackage();
+        savePackageName(context, mAppWidgetId, packageName);
+
+        // It is the responsibility of the configuration activity to update the app widget
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+        AppIconWidget.updateAppWidget(context, appWidgetManager, mAppWidgetId);
+
+        // Make sure we pass back the original appWidgetId
+        Intent resultValue = new Intent();
+        resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
+        setResult(RESULT_OK, resultValue);
+        finish();
+
+
+    }
+    /**
+     * Displays either the recyclerView or the progressbar depending upon showRV
+     *
+     * @param showRV decides which view to be displayed
+     */
+    private void showRecyclerView(boolean showRV) {
+        if (showRV) {
+
+            mAppsRecyclerView.setVisibility(View.VISIBLE);
+            mLoadingIndicator.setVisibility(View.GONE);
+        } else {
+            mAppsRecyclerView.setVisibility(View.GONE);
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private LoaderManager.LoaderCallbacks<ArrayList> fetchAppsListener
+            = new LoaderManager.LoaderCallbacks<ArrayList>() {
+        @NonNull
+        @Override
+        public Loader<ArrayList> onCreateLoader(int id, @Nullable Bundle args) {
+            Timber.d("onCreateLoader");
+            showRecyclerView(false);
+            return new AppLoader(AppIconWidgetConfigureActivity.this, id);
+        }
+
+        /**
+         * Initialises the RecyclerView displaying the list of apps
+         */
+        @Override
+        public void onLoadFinished(@NonNull Loader<ArrayList> loader, ArrayList data) {
+            showRecyclerView(true);
+            Timber.d("onLoadFinished");
+            mAdapter = new AppAdapter(AppIconWidgetConfigureActivity.this, AppIconWidgetConfigureActivity.this);
+            mAppsRecyclerView.setAdapter(mAdapter);
+
+            mAdapter.setListData(data);
+
+            mAppsRecyclerView.setLayoutManager(new LinearLayoutManager(AppIconWidgetConfigureActivity.this));
+            mAppsRecyclerView.setHasFixedSize(true);
+
+            getSupportLoaderManager().destroyLoader(APPS_LOADER_MANAGER_ID);
+        }
+
+        @Override
+        public void onLoaderReset(@NonNull Loader<ArrayList> loader) {
+
+        }
+    };
+
+
+    public static class AppLoader extends AsyncTaskLoader<ArrayList> {
+
+        int mId;
+
+        public AppLoader(@NonNull Context context, int id) {
+            super(context);
+            mPackageManager = context.getPackageManager();
+            mId = id;
+        }
+
+        /**
+         * Loads a list of installed apps on the device using PackageManager
+         */
+        @Nullable
+        @Override
+        public ArrayList loadInBackground() {
+            Timber.d("loadInBackground");
+            ArrayList mAppsList = new ArrayList<>();
+            if (mId == APPS_LOADER_MANAGER_ID) {
+
+                Intent intent = new Intent(Intent.ACTION_MAIN, null);
+                intent.addCategory(Intent.CATEGORY_LAUNCHER);
+                List<ResolveInfo> activities = mPackageManager.queryIntentActivities(intent, 0);
+                Collections.sort(activities, new ResolveInfo.DisplayNameComparator(mPackageManager));
+                for (ResolveInfo ri : activities) {
+                    ContentValues values = new ContentValues();
+                    App app = new App();
+                    app.setmPackage(ri.activityInfo.packageName);
+                    app.setmTitle((String) ri.loadLabel(mPackageManager));
+                    app.setmIcon(ri.activityInfo.loadIcon(mPackageManager));
+                    mAppsList.add(app);
+                    values.put(AppColumns.APP_TITLE, app.getmTitle());
+                    values.put(AppColumns.PACKAGE_NAME, app.getmPackage());
+                    getContext().getContentResolver().insert(URI_APPS, values);
+                }
+
+
+            } else if (mId == APPS_LOADER_DB_ID) {
+
+                Cursor cursor = getContext().getContentResolver().query(URI_APPS, null, null, null, null);
+                if (cursor != null) {
+
+                    cursor.moveToFirst();
+                    while (!cursor.isAfterLast()) {
+                        App app = new App();
+                        app.setmTitle(cursor.getString(cursor.getColumnIndexOrThrow(AppColumns.APP_TITLE)));
+                        app.setmPackage(cursor.getString(cursor.getColumnIndexOrThrow(AppColumns.PACKAGE_NAME)));
+                        try {
+                            app.setmIcon(mPackageManager.getApplicationIcon(app.getmPackage()));
+
+                        } catch (PackageManager.NameNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                        mAppsList.add(app);
+                        cursor.moveToNext();
+
+                    }
+                    cursor.close();
+                }
+            }
+
+            return mAppsList;
+        }
+
+        @Override
+        protected void onStartLoading() {
+
+            Timber.d("onStartLoading");
+            forceLoad();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private static final String PREFS_NAME = "com.addie.maxfocus.widget.AppIconWidget";
+    private static final String PREF_PREFIX_KEY = "appwidget_";
+    int mAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
+//    EditText mAppWidgetText;
+   /* View.OnClickListener mOnClickListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            final Context context = AppIconWidgetConfigureActivity.this;
+
+            // When the button is clicked, store the string locally
+            String widgetText = mAppWidgetText.getText().toString();
+            savePackageName(context, mAppWidgetId, widgetText);
+
+            // It is the responsibility of the configuration activity to update the app widget
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            AppIconWidget.updateAppWidget(context, appWidgetManager, mAppWidgetId);
+
+            // Make sure we pass back the original appWidgetId
+            Intent resultValue = new Intent();
+            resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
+            setResult(RESULT_OK, resultValue);
+            finish();
+        }
+    };*/
+
+    public AppIconWidgetConfigureActivity() {
+        super();
+    }
+
+    // Write the prefix to the SharedPreferences object for this widget
+    static void savePackageName(Context context, int appWidgetId, String text) {
+        SharedPreferences.Editor prefs = context.getSharedPreferences(PREFS_NAME, 0).edit();
+        prefs.putString(PREF_PREFIX_KEY + appWidgetId, text);
+        prefs.apply();
+    }
+
+    // Read the prefix from the SharedPreferences object for this widget.
+    // If there is no preference saved, get the default from a resource
+    static String loadPackageNameForWidget(Context context, int appWidgetId) {
+        SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, 0);
+        String titleValue = prefs.getString(PREF_PREFIX_KEY + appWidgetId, null);
+        if (titleValue != null) {
+            return titleValue;
+        } else {
+            return context.getString(R.string.appwidget_text);
+        }
+    }
+
+    static void deleteTitlePref(Context context, int appWidgetId) {
+        SharedPreferences.Editor prefs = context.getSharedPreferences(PREFS_NAME, 0).edit();
+        prefs.remove(PREF_PREFIX_KEY + appWidgetId);
+        prefs.apply();
+    }
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        // Set the result to CANCELED.  This will cause the widget host to cancel
+        // out of the widget placement if the user presses the back button.
+        setResult(RESULT_CANCELED);
+
+        setContentView(R.layout.app_icon_widget_configure);
+//        mAppWidgetText = (EditText) findViewById(R.id.appwidget_text);
+//        findViewById(R.id.add_button).setOnClickListener(mOnClickListener);
+
+        //Code from AppsActivity
+        ButterKnife.bind(this);
+
+        // Start Loading Apps
+        loadAppsFromManagerOrDb();
+
+        //Register broadcast receiver to receive "stop app" dialogs
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ACTION_APP_DIALOG);
+        mAppDialogBroadcastReceiver = new AppDialogBroadcastReceiver();
+        registerReceiver(mAppDialogBroadcastReceiver, filter);
+
+
+
+        // Find the widget id from the intent.
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            mAppWidgetId = extras.getInt(
+                    AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+        }
+
+        // If this activity was started with an intent without an app widget ID, finish with an error.
+        if (mAppWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
+            finish();
+            return;
+        }
+
+//        mAppWidgetText.setText(loadPackageNameForWidget(AppIconWidgetConfigureActivity.this, mAppWidgetId));
+    }
+}
+
