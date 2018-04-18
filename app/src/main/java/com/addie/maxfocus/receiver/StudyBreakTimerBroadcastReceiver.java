a20b47477adcc3847b9198afd795132ee583fa4a
Aditya <addiegupta@gmail.com>
2018-04-18 23:11:26 +0530

    Add TODO

--- app/src/main/java/com/addie/maxfocus/receiver/StudyBreakTimerBroadcastReceiver.java
+++ app/src/main/java/com/addie/maxfocus/receiver/StudyBreakTimerBroadcastReceiver.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 
 
+//TODO Implement to start new timer
 public class StudyBreakTimerBroadcastReceiver extends BroadcastReceiver {
 
     private static final String ACTION_STUDY_BREAK_RECEIVER = "com.addie.maxfocus.service.action.STUDY_BREAK";
